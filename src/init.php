2bf1d6cc5440a83d2f1c6e6ca853428d94128c58
Mithun Biswas <bhoot.biswas@gmail.com>
2019-11-07 12:19:54 +0600

    Enqueue block and block editor assets

--- src/init.php
+++ src/init.php
@@ -16,10 +16,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Enqueue Gutenberg block assets for both frontend + backend.
  *
- * Assets enqueued:
- * 1. blocks.style.build.css - Frontend + Backend.
- * 2. blocks.build.js - Backend.
- * 3. blocks.editor.build.css - Backend.
+ * @uses {wp-editor} for WP editor styles.
+ * @since 1.0.0
+ */
+function wp_job_manager_blocks_block_assets() {
+	// Styles.
+	wp_enqueue_style(
+		'wp_job_manager_blocks-style-css', // Handle.
+		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
+		array( 'wp-editor' ), // Dependency to include the CSS after it.
+		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
+	);
+} // End function wp_job_manager_blocks_block_assets().
+
+// Hook: Frontend assets.
+add_action( 'enqueue_block_assets', 'wp_job_manager_blocks_block_assets' );
+
+/**
+ * Enqueue Gutenberg block assets for backend editor.
  *
  * @uses {wp-blocks} for block type registration & related functions.
  * @uses {wp-element} for WP Element abstraction — structure of blocks.
@@ -27,64 +41,22 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @uses {wp-editor} for WP editor styles.
  * @since 1.0.0
  */
-function wp_job_manager_blocks_cgb_block_assets() { // phpcs:ignore
-	// Register block styles for both frontend + backend.
-	wp_register_style(
-		'wp_job_manager_blocks-cgb-style-css', // Handle.
-		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
-		array( 'wp-editor' ), // Dependency to include the CSS after it.
-		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
-	);
-
-	// Register block editor script for backend.
-	wp_register_script(
-		'wp_job_manager_blocks-cgb-block-js', // Handle.
+function wp_job_manager_blocks_editor_assets() {
+	// Scripts.
+	wp_enqueue_script(
+		'wp_job_manager_blocks-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
-		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
-		true // Enqueue the script in the footer.
+		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ) // Version: File modification time.
 	);
-
-	// Register block editor styles for backend.
-	wp_register_style(
-		'wp_job_manager_blocks-cgb-block-editor-css', // Handle.
+	// Styles.
+	wp_enqueue_style(
+		'wp_job_manager_blocks-block-editor-css', // Handle.
 		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
 		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
-		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
+		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
 	);
+} // End function wp_job_manager_blocks_editor_assets().
 
-	// WP Localized globals. Use dynamic PHP stuff in JavaScript via `cgbGlobal` object.
-	wp_localize_script(
-		'wp_job_manager_blocks-cgb-block-js',
-		'cgbGlobal', // Array containing dynamic data for a JS Global.
-		[
-			'pluginDirPath' => plugin_dir_path( __DIR__ ),
-			'pluginDirUrl'  => plugin_dir_url( __DIR__ ),
-			// Add more data here that you want to access from `cgbGlobal` object.
-		]
-	);
-
-	/**
-	 * Register Gutenberg block on server-side.
-	 *
-	 * Register the block on server-side to ensure that the block
-	 * scripts and styles for both frontend and backend are
-	 * enqueued when the editor loads.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type#enqueuing-block-scripts
-	 * @since 1.16.0
-	 */
-	register_block_type(
-		'cgb/block-wp-job-manager-blocks', array(
-			// Enqueue blocks.style.build.css on both frontend & backend.
-			'style'         => 'wp_job_manager_blocks-cgb-style-css',
-			// Enqueue blocks.build.js in the editor only.
-			'editor_script' => 'wp_job_manager_blocks-cgb-block-js',
-			// Enqueue blocks.editor.build.css in the editor only.
-			'editor_style'  => 'wp_job_manager_blocks-cgb-block-editor-css',
-		)
-	);
-}
-
-// Hook: Block assets.
-add_action( 'init', 'wp_job_manager_blocks_cgb_block_assets' );
+// Hook: Editor assets.
+add_action( 'enqueue_block_editor_assets', 'wp_job_manager_blocks_editor_assets' );
